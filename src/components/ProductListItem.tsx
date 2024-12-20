7fa6ae4dcbf7e70a4b424d4cfba73c7f66757c7d
pritam kumar <pritam18jan@gmail.com>
2024-12-20 12:11:22 +0530

    wrong commit

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,18 +1,16 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import { Product } from '../types';
 
-export const defaultPizzaImage =
-  'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+interface Product {
+    image: string;
+    name: string;
+    price: number;
+  }
 
-type ProductListItemProps = {
-    product: Product;
-  };
-
-const ProductListItem = ({ product }: ProductListItemProps)  => {
+const ProductListItem = ({ product }: { product: Product }) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage  }} style={styles.image} resizeMode="contain" />
+      <Image source={{ uri: product.image }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
