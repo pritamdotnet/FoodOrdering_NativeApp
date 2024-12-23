de57de89160e749f33f326ba4da466f86a94892c
pritam kumar <pritam18jan@gmail.com>
2024-12-23 12:13:03 +0530

    Expo Router -- Menu

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,5 @@
-import { View } from 'react-native';
-import products from '@assets/data/products';
-import ProductListItem from '@components/ProductListItem';
+import { Redirect } from 'expo-router';
 
-export default function MenuScreen() {
-  return (
-    <View >
-     <ProductListItem product={products[5]} />
-     <ProductListItem product={products[1]} />
-     </View>
-  );
-}
-
+export default function TabIndex () {
+  return <Redirect href={'/menu/'} />;
+};
